3058610a755ecf05e6be89e79564195151664b47
Bart Kessels <bart.kessels@ns.nl>
2024-10-18 13:49:05 +0200

    Move

--- src/Common/Helpers/MessagingFactory.cs
+++ src/Common/Helpers/MessagingFactory.cs
@@ -1,8 +1,47 @@
 ﻿namespace ServiceBusExplorer
 {
+    using Azure.Core;
+    using Azure.Messaging.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+
     public class MessagingFactory
     {
-        public void CreateQueueClient()
+        private readonly ServiceBusClient _client;
+
+        private MessagingFactory(string fullyQualifiedNamespace, TokenCredential tokenCredential)
+        {
+            _client = new ServiceBusClient(fullyQualifiedNamespace, tokenCredential);
+        }
+
+        private MessagingFactory(string connectionString)
+        {
+            _client = new ServiceBusClient(connectionString);
+        }
+
+        public static MessagingFactory CreateFromConnectionString(string connectionString)
+        {
+            return new MessagingFactory(connectionString);
+        }
+
+        public static MessagingFactory CreateFromTokenCredential(string fullyQualifiedNamespace, TokenCredential tokenCredential)
+        {
+            return new MessagingFactory(fullyQualifiedNamespace, tokenCredential);
+        }
+
+        public QueueClient CreateQueueClient(string path, ServiceBusReceiveMode receiveMode)
+        {
+            return new QueueClient(_client, path, receiveMode);
+        }
+
+        public ServiceBusReceiver CreateMessageReceiver(string path, ServiceBusReceiveMode receiveMode)
+        {
+            return _client.CreateReceiver(path, new ServiceBusReceiverOptions
+            {
+                ReceiveMode = receiveMode
+            });
+        }
+
+        public void CreateSubscriptionClient(string topicPath, string subscriptionName)
         {
             
         }
