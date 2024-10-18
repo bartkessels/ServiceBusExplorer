7a3e5961a4461865695806469b2a84f03fd32798
Amzir-A <102997068+Amzir-A@users.noreply.github.com>
2024-10-18 13:42:35 +0200

    Create QueueClient Constructor

--- src/ServiceBusExplorer/QueueClient.cs
+++ src/ServiceBusExplorer/QueueClient.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.NotificationHubs;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Management.Instrumentation;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace ServiceBusExplorer
 {
     public class QueueClient
     {
+        private readonly ServiceBusClient _serviceBusClient;
+        public QueueClient(ServiceBusClient serviceBusClient)
+        {
+            _serviceBusClient = serviceBusClient;
+        }
     }
 }
